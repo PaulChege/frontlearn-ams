c86963d87957ce30522d8d9aa8ca8dc5cd32856d
paulchege <pchegenjenga@gmail.com>
2020-02-19 12:46:52 +0300

    Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  full_name: "First Admin", 
+  email: "admin@frontlearn.co.ke",
+  role: :admin,
+  password: "frontlearn16" 
+)
+
+["Culinary Arts", "ICT", "Business"].each do |school|
+  school = School.find_or_create_by(name: school)
+end
+
