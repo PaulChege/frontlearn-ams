efe08ac1903c1682e8dc9b4edf11c40d62d410f3
paulchege <pchegenjenga@gmail.com>
2020-02-19 13:02:26 +0300

    Changed migrations

--- db/migrate/20200102175829_add_columns_to_courses.rb
+++ db/migrate/20200102175829_add_columns_to_courses.rb
@@ -2,7 +2,6 @@
 
 class AddColumnsToCourses < ActiveRecord::Migration[6.0]
   def change
-    add_column :courses, :name, :string
     add_column :courses, :level, :integer
     remove_column :courses, :CreateCourses, :string
   end
