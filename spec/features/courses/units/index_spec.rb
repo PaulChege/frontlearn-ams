2be216ee3630cdebb2cea85f404c7454e3762fce
PaulChege <pchegenjenga@gmail.com>
2020-03-26 10:46:32 +0300

    Added feature tests for units

---
+++ spec/features/courses/units/index_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe 'course units index page' do
+  login_user
+
+  before(:each) do
+    Capybara.current_driver = :selenium_chrome
+    @course = create(:course)
+    visit school_course_units_path(@course.school, @course)
+  end
+
+  scenario 'opening manage all units page' do
+    click_link('Manage All Units')
+    expect(page).to have_content('Units for All Courses')
+  end
+
+  scenario 'creating a new unit' do
+    fill_in('Code', with: 'ICT1')
+    fill_in('Name', with: 'Introduction to Programming')
+    click_button('Create')
+    expect(page).to have_content('Unit created and added to course')
+    expect(page).to have_content('Introduction to Programming')
+  end
+
+  scenario 'adding an existing unit' do
+    @unit = create(:unit)
+    select(@unit.full_unit_name, from: 'Unit')
+    click_link('Add')
+    expect(page).to have_content(@unit.full_unit_name)
+  end
+end
---
+++ spec/features/units/index_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe 'all units index page' do
+  login_user
+
+  before(:each) do
+    @unit =  create(:unit)
+    Capybara.current_driver = :selenium_chrome
+    visit units_path    
+  end
+
+  scenario 'rendering index page' do
+    expect(page).to have_content('Units for All Courses')
+  end
+
+  scenario 'opening edit page' do
+    find_link(href: "/units/#{@unit.id}/edit").click
+    expect(page).to have_content('Edit Unit')
+  end
+
+  scenario 'deleting unit' do
+    find_link(href: "/units/#{@unit.id}").click
+    click_button('Yes')
+    expect(page).not_to have_content(@unit.full_unit_name)
+  end
+end
